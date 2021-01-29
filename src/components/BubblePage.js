746ebd02bf4196eb8dac0cc32c26748ed9ce85e9
Rafael Molina <seissaysseis@users.noreply.github.com>
2021-01-29 11:49:55 -0700

    get colors and private routes needed still

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from '../helpers/axiosWithAuth'
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+    .get("/api/colors")
+    .then((res) => {
+      setColorList(res.data)
+    })
+    .catch((err) => {
+      console.log(err, "alert")
+    })
+  },[]);
 
   return (
     <>
